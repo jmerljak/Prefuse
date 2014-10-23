0efe0970d1610513fabe5d03261285d9c77e559c
Jakob Merljak <jakob.merljak@optilab.net>
2014-10-23 11:29:39 +0200

    removed demo package - moved to separate project prefuse-demo

--- test/test/prefuse/data/All_PrefuseData_Tests.java
+++ test/test/prefuse/data/All_PrefuseData_Tests.java
@@ -12,7 +12,6 @@ public class All_PrefuseData_Tests {
         suite.addTestSuite(CascadedTableTest.class);
         suite.addTestSuite(TableTest.class);
         suite.addTestSuite(GraphTest.class);
-        suite.addTestSuite(TreeTest.class);
         //$JUnit-END$
         return suite;
     }
